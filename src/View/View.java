fa946edf4f8b2a6f400a51676c18b2caa8e52444
Emil Krastev <Emil.Krastev@skrill.com>
2013-06-03 11:48:21 +0300

    Added View class

---
+++ src/View/View.java
@@ -0,0 +1,18 @@
+package View;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+
+
+
+
+public class View {
+
+public static void main(String args[]){
+	
+		
+		
+		
+		
+	}
+}
